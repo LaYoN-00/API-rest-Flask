8978dd91c18f72a9b9eb2b3a333b4fb90fbdfc23
LaYoN-00 <diego.mercado1066@alumnos.udg.mx>
2024-06-11 09:12:45 -0600

    ACTUALIZACION

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from config import config
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 app=Flask(__name__)
 conexion=MySQL(app)
 # Configura CORS para permitir todas las solicitudes desde cualquier origen
 CORS(app)
+
+
+#RUTAS PARA HACER CRUD SOBRE ALGUNA TABLA DE LA DB
 
 @app.route('/api/cursos/listar', methods=['GET'])
 
@@ -20,7 +28,7 @@ def listar_cursos():
         for fila in datos:
             curso={'codigo':fila[0],'materia':fila[1],'creditos':fila[2],}
             cursos.append(curso)
-        return jsonify({'cursos':cursos,'mensaje':"Cursos Listados"})
+        return jsonify(cursos)
     except Exception as ex:
         return "ERROR"
     
@@ -35,7 +43,7 @@ def leer_curso(codigo):
 
         if datos is not None:
             curso={'codigo':datos[0],'materia':datos[1],'creditos':datos[2],}
-            return jsonify({'curso':curso,'mensaje':"Curso Econtrado"})
+            return jsonify(curso)
         
         else:
             return jsonify({'mensaje':"Curso NO Encontrado"})
@@ -83,6 +91,47 @@ def actualizar_curso(codigo):
 
     except Exception as ex:
         return "ERROR"
+    
+#RUTAS PARA USAR EL ARBOL DE DECISION
+@app.route('/api/arbol-decision/global',methods=['GET'])
+
+def ArbolDecisionGlobal():
+    try:
+        cursor=conexion.connection.cursor()
+        sql="SELECT numeros_primos, tiempo_numeros_primos, criterios_de_divisibilidad, tiempo_criterios_divisibilidad, ecuaciones_cuadraticas, tiempo_ecuaciones_cuadraticas, teorema_de_pitagoras, tiempo_teorema_pitagoras, algebra, tiempo_algebra, funciones, tiempo_funciones, trigonometria, tiempo_trigonometria, geometria, tiempo_geometria, calculo, tiempo_calculo, tema_tipo_refuerzo FROM calificaciones_examenes"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        # Obteniendo los nombres de las columnas
+        columnas = [desc[0] for desc in cursor.description]
+        # Creando el DataFrame
+        df = pd.DataFrame(datos, columns=columnas)
+        # Supongamos que 'mi_objetivo' es la columna que quieres predecir y el resto son las características
+        X = df.drop('tema_tipo_refuerzo', axis=1)
+        y = df['tema_tipo_refuerzo']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
+        arbol = DecisionTreeClassifier(criterion='entropy', max_depth=4)
+        # Entrenar el modelo con el conjunto de entrenamiento
+        arbol.fit(X_train, y_train)
+        # Realizar predicciones en el conjunto de prueba
+        y_pred = arbol.predict(X_test)
+        print("PREDICCION DEL MODELO: ", y_pred[0:5])
+        # Calcular la precisión
+        precision = accuracy_score(y_test, y_pred)
+        print("La precisión del modelo es: ", precision)
+        # Crear un diccionario con los resultados
+        resultados = {
+            "prediccion": y_pred.tolist(),  # Convertir el array numpy a lista
+            "precision": precision
+        }
+
+        # Retornar los resultados en formato JSON
+        return jsonify(resultados)
+    
+    except Exception as ex:
+        return "ERROR"
+    
+@app.route('/api/arbol-decision/local',methods=['GET'])
+
 
 def pagina_no_encontada(error):
     return "<h1>PAGINA NO ENCONTRADA...</h1>",404
